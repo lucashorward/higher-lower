4d89429a85cf682a3c2f91bdfe92214b004e3fe3
Lucas Horward <l.horward@essent.nl>
2024-02-13 17:31:19 +0100

    Add tests, reduce code duplication

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
 	"math/rand"
 	"net/http"
+	"strconv"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 )
 
@@ -19,8 +22,8 @@ const (
 )
 
 type newGame struct {
-	Min int `json:"min"`
-	Max int `json:"max"`
+	Min int `json:"min" validate:"required"`
+	Max int `json:"max" validate:"required"`
 }
 
 type game struct {
@@ -29,11 +32,13 @@ type game struct {
 	Ended   bool   `json:"ended"`
 	answer  int
 	Message string `json:"message"`
+	min     int
+	max     int
 }
 
 type guess struct {
-	GameId string `json:"gameId"`
-	Guess  int    `json:"guess"`
+	GameId string `json:"gameId" validate:"required"`
+	Guess  int    `json:"guess" validate:"required"`
 }
 
 func (result Result) String() string {
@@ -44,17 +49,23 @@ func (result Result) EnumIndex() int {
 	return int(result)
 }
 
+// Saving the games in RAM means we're fast but also super super vulnerable to DOS attacks
+// So this should probably not go online without throttling logic
+
+// All games, stored as a map of <gameId, *game>
 var games = make(map[string]*game)
 
-func createGameHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	newGame := &newGame{}
-	err := decoder.Decode(newGame)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+// Core creation functionality.
+// Creates a game, adds it to the games array, then returns it
+//
+// @throws if the Max is not higher than the Min value
+func CreateGame(inputData *newGame) (*game, error) {
+	if inputData.Min >= inputData.Max {
+		return &game{}, errors.New("Max must be higher than the Min")
 	}
+
 	gameId := uuid.NewString()
-	answer := rand.Intn(newGame.Max-newGame.Min) + newGame.Min
+	answer := rand.Intn(inputData.Max-inputData.Min) + inputData.Min
 
 	createdGame := &game{
 		gameId,
@@ -62,37 +73,50 @@ func createGameHandler(w http.ResponseWriter, r *http.Request) {
 		false,
 		answer,
 		"Welcome to the game!",
+		inputData.Min,
+		inputData.Max,
 	}
 	games[gameId] = createdGame
+	return createdGame, nil
+}
+
+// CreateGame, wrapped into a HTTP endpoint
+func createGameHandler(w http.ResponseWriter, r *http.Request, newGame *newGame) {
+	createdGame, gameErr := CreateGame(newGame)
+	if gameErr != nil {
+		http.Error(w, gameErr.Error(), http.StatusInternalServerError)
+		return
+	}
 	jsonResponse, _ := json.Marshal(createdGame)
 	w.Write(jsonResponse)
 }
 
-func guessHandler(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	guess := &guess{}
-	err := decoder.Decode(guess)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	savedGame, exists := games[guess.GameId]
+/*
+- Core guessing functionality.
+
+- @throws if the provided gameId is not in the games map
+
+- @throws if the guess is not between the min and max of the game
+*/
+func Guess(inputGuess *guess) (*game, error) {
+	savedGame, exists := games[inputGuess.GameId]
 	if !exists {
-		http.Error(w, "The requested game does not exist", http.StatusBadRequest)
-		return
+		return &game{}, errors.New("The requested game does not exist")
 	}
 	if savedGame.Ended {
-		jsonResponse, _ := json.Marshal(savedGame)
-		w.Write(jsonResponse)
-		return
+		return savedGame, nil
+	}
+	if inputGuess.Guess > savedGame.max || inputGuess.Guess < savedGame.min {
+		return &game{}, errors.New("Guess is out of bounds, must be between " + strconv.Itoa(savedGame.min) + " and " + strconv.Itoa(savedGame.max))
 	}
 	var result Result
 	ended := false
 	var message string
-	if guess.Guess == savedGame.answer {
+	if inputGuess.Guess == savedGame.answer {
 		result = Equal
 		ended = true
 		message = "Excelsior, you win!!"
-	} else if guess.Guess > savedGame.answer {
+	} else if inputGuess.Guess > savedGame.answer {
 		result = TooHigh
 		message = "Fiddlesticks, that's too high"
 	} else {
@@ -105,9 +129,46 @@ func guessHandler(w http.ResponseWriter, r *http.Request) {
 		ended,
 		savedGame.answer,
 		message,
+		savedGame.min,
+		savedGame.max,
 	}
 	games[savedGame.Id] = updatedGame
-	jsonResponse, _ := json.Marshal(updatedGame)
+	return updatedGame, nil
+}
+
+func decodeAndValidate[T any](inputFunction func(http.ResponseWriter, *http.Request, T)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		object := new(T)
+		err := decoder.Decode(object)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		validate := validator.New(validator.WithRequiredStructEnabled())
+		validationError := validate.Struct(*object)
+		if validationError != nil {
+			http.Error(w, validationError.Error(), http.StatusBadRequest)
+			return
+		}
+		inputFunction(w, r, *object)
+	}
+}
+
+// Guess endpoint
+func guessHandler(w http.ResponseWriter, r *http.Request, guess *guess) {
+	game, err := Guess(guess)
+	if err != nil {
+		var status int
+		if err.Error() == "The requested game does not exist" {
+			status = http.StatusBadRequest
+		} else {
+			status = http.StatusInternalServerError
+		}
+		http.Error(w, err.Error(), status)
+		return
+	}
+	jsonResponse, _ := json.Marshal(game)
 	w.Write(jsonResponse)
 }
 
@@ -118,8 +179,8 @@ func allGamesHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Println("Starting, stand by")
-	http.HandleFunc("POST /game", createGameHandler)
-	http.HandleFunc("POST /guess", guessHandler)
+	http.HandleFunc("POST /game", decodeAndValidate[*newGame](createGameHandler))
+	http.HandleFunc("POST /guess", decodeAndValidate[*guess](guessHandler))
 	http.HandleFunc("GET /games", allGamesHandler)
 
 	log.Println("Server is up! Go play :)")
---
+++ main_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGameCreation(t *testing.T) {
+	input := newGame{
+		Min: 0,
+		Max: 100,
+	}
+	response, err := CreateGame(&input)
+	if err != nil {
+		t.Fatalf(`CreateGame = %v, %q, don't want an error`, response, err)
+	}
+	if response.Result != "None" {
+		t.Fatalf(`CreateGame = %v, want "None"`, response)
+	}
+	if response.Id == "" {
+		t.Fatalf(`CreateGame = %v, want a filled in game ID"`, response)
+	}
+	if response.Ended == true {
+		t.Fatalf(`CreateGame = %v, want the ended to be preset to false"`, response)
+	}
+	if response.answer > 100 || response.answer < 0 {
+		t.Fatalf(`CreateGame = %v, want the answer to be in the given min and max"`, response)
+	}
+	if response.Message != "Welcome to the game!" {
+		t.Fatalf(`CreateGame = %v, expect the default message"`, response)
+	}
+}
+
+func TestGameCreationInvalidBoundaries(t *testing.T) {
+	input := newGame{
+		Min: 100,
+		Max: 0,
+	}
+	response, err := CreateGame(&input)
+	if err == nil {
+		t.Fatalf(`CreateGame = %v, %q, want error to be returned`, response, err)
+	}
+	if err.Error() != "Max must be higher than the Min" {
+		t.Fatalf(`CreateGame = %v, %q, want error to be correct`, response, err)
+	}
+}
+
+func TestGameCreationEqualBoundaries(t *testing.T) {
+	input := newGame{
+		Min: 100,
+		Max: 100,
+	}
+	response, err := CreateGame(&input)
+	if err == nil {
+		t.Fatalf(`CreateGame = %v, %q, want error to be returned`, response, err)
+	}
+	if err.Error() != "Max must be higher than the Min" {
+		t.Fatalf(`CreateGame = %v, %q, want error to be correct`, response, err)
+	}
+}
+
+func TestGuessNonExistingGame(t *testing.T) {
+	input := &guess{
+		GameId: "Fake",
+		Guess:  5000,
+	}
+	response, err := Guess(input)
+	if err == nil {
+		t.Fatalf(`Guess = %v, %q, want error to be returned, error`, response, err)
+	}
+	if err.Error() != "The requested game does not exist" {
+		t.Fatalf("Error must be about the game not existing")
+	}
+}
+
+func TestGuessOutOfBounds(t *testing.T) {
+	input := newGame{
+		Min: 0,
+		Max: 100,
+	}
+	response, _ := CreateGame(&input)
+	guessInput := &guess{
+		GameId: response.Id,
+		Guess:  5000,
+	}
+	response, err := Guess(guessInput)
+	if err == nil {
+		t.Fatalf(`Guess = %v, %q, want error to be returned`, response, err)
+	}
+	if err.Error() != "Guess is out of bounds, must be between 0 and 100" {
+		t.Fatalf("Guess Error must be about the guess being Out Of Bounds")
+	}
+}
